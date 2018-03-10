64bd947eb4e5a36bafa0ff30a6667b2270e1ab1d
badtudou <badtudou@gmail.com>
2018-03-10 21:58:16 +0800

    read floppy

--- bootstrap/bootstrap.asm
+++ bootstrap/bootstrap.asm
@@ -29,6 +29,13 @@
 ;	| (not used)
 ;	+--------------------- (...)
 
+;   Disk
+;   -------------
+;   磁头(Heads)
+;   柱面(Cylinder)
+;   扇区(Sector)
+;   -------------
+;   Heads * Cylinder * Sector * 512B
 
 MBR:
     code:
@@ -36,7 +43,8 @@ MBR:
 
         mov ax, cs
         mov ds, ax
-        mov si, data
+        ;mov si, hello
+        jmp Read_Floppy
 
 
         Print_String:
@@ -50,11 +58,40 @@ MBR:
             int 10h
             jmp Print_String
 
+                    
+        Read_Floppy: ;ES:BX   Buffer Address Pointer
+            mov ax, 0820h
+            mov es, ax
+            mov bx, 0h
+            mov ah, 02h ;功能：Read Sectors From Drive
+            mov cl, 01h ;要读取的扇区个数Sectors To Read Count
+            mov ch, 0h  ;柱面Cylinder
+            mov dh, 0h  ;磁头Head
+            mov al, 02h ;扇区Sector
+            mov dl, 0h  ;设备Drive，1st floppy disk ( "drive A:" )
+            int 13h
+
+        ReadFloppyOK:
+            jc ReadFloppyError
+            mov si, ok
+            jmp Print_String
+        ReadFloppyError:
+            mov si, error
+            jmp Print_String
+
+
         End:
             hlt
     data:
+        hello:
         db    "Hello, OS world!"
         db     0h   ;字符串结束标志
+        ok:
+        db      "Ok!"
+        db      0h
+        error:
+        db      "Error!"
+        db      0h
     
     times 510-($-$$) db    0    ; 当前位置到510，用0填充
 
@@ -62,3 +99,5 @@ MBR:
         dw 0xaa55    ; MBR签名
 
 ;Why BIOS loads MBR into 0x7C00 in x86 ? https://www.glamenv-septzen.net/en/view/6
+
+;https://www.wikiwand.com/en/INT_13H
